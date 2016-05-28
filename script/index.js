cda7cc47b0ad49798e27da113a0803bd3b7c3c44
zhangzongshan <zhangzongshan@126.com>
2016-05-29 00:07:21 +0800

    shouye

--- script/index.js
+++ script/index.js
@@ -4,11 +4,39 @@
 'use stice';
 define(function (require, exports, module) {
 
-    seajs.use(['style/head/head.css']);
+    seajs.use([
+        'style/head/head.css'
+        ,'style/content.css'
+        ,'style/foot/foot.css'
+    ]);
     DataLoad.Debug(true);
     var headObj = {
         init: function (container) {
             DataLoad.GetFile('HeadHtml', 'html/head/head.html', function (html) {
+                if (html != '') {
+                    container.html(html);
+                    container.css({
+                        'z-index':common.fn.getmaxZindex()
+                    });
+                }
+            });
+        }
+    }
+    
+    var footObj={
+        init:function (container) {
+            DataLoad.GetFile('FootHtml', 'html/foot/foot.html', function (html) {
+                if (html != '') {
+                    container.html(html);
+                }
+            });
+        }
+    }
+
+
+    var contenObj={
+        init:function (container) {
+            DataLoad.GetFile('ContentHtml', 'html/content/index.html', function (html) {
                 if (html != '') {
                     container.html(html);
                 }
@@ -18,6 +46,11 @@ define(function (require, exports, module) {
 
     function init() {
         headObj.init($('#head'));
+        contenObj.init($('#content'));
+        footObj.init($('#foot'));
+        $('#content').css({
+            'min-height':($(window).height()-180)+'px'
+        });
     }
 
     return {
