1c19cb23a839262c732bf5bc0978448c118865f7
zhangzongshan <zhangzongshan@126.com>
2016-06-15 11:08:29 +0800

    init

--- _assets/js/plugins.js
+++ _assets/js/plugins.js
@@ -24,11 +24,29 @@
 }());
 
 (function () {
+    function getScriptRoot(scriptName) {
+        var scripts = document.getElementsByTagName("script");
+        for (var n = 0; n < scripts.length; n++) {
+            var script = scripts[n];
+            if (script.src.match(eval("/" + scriptName + "(\.min|)\.js/"))) {
+                return (script.src).split("/").slice(0, -1).join("/") + "/";
+            }
+        }
+        return "";
+    }
     var sysSpinkit = null;
+    var rootpath = getScriptRoot('plugins').replace('_assets/js/', '');
+    var loadMapUrl = '';
+    var loadMapFlg = false;
+    if (window.location.href.replace(rootpath, '') == '' || window.location.href.replace(rootpath, '').indexOf('index.html') == 0) {
+        loadMapUrl = 'http://api.map.baidu.com/api?v=2.0&ak=e35biu9blkyBwcyfYBXaI5zfwyMRjLSU&callback=mapinit';
+        loadMapFlg = true;
+    }
+    console.log(loadMapUrl);
     Modernizr.load([{
         load: {
-            'jquery': '_assets/js/public/jquery/jquery-1.12.3.min.js'//jquery
-            , 'spink': '_assets/js/module/spinkit/spinkit.js'//加载动画
+            'jquery': rootpath + '_assets/js/public/jquery/jquery-1.12.3.min.js'//jquery
+            , 'spink': rootpath + '_assets/js/module/spinkit/spinkit.js'//加载动画
         },
         callback: function (url, result, key) {
             //alert(url);
@@ -40,17 +58,16 @@
         }
     }, {
         load: {
-            'lodash': '_assets/js/public/underscore/underscore.js'//基础库
-            , 'html5media': '_assets/js/public/html5media/html5media-1.1.8.min.js'//多媒体
-            , 'moment': '_assets/js/public/moment/moment-local-2.1.3.min.js'//日期
-            , 'validator': '_assets/js/public/validator/validator-5.2.0.min.js'//验证
-            , 'common': '_assets/js/module/common/common.js'//加载通用函数
-            , 'noty': '_assets/js/module/notykit/notykit.js'//对话框
-            , 'dataload': '_assets/js/module/dataload/dataload.js'//数据请求
-            , 'seajs': '_assets/js/frame/seajs/sea.js'//
-            , 'seajs-css': '_assets/js/frame/seajs/seajs-css.js'//
-            , 'seajs-preload': '_assets/js/frame/seajs/seajs-preload.js'//数据请求
-            , 'baiduMao': 'http://api.map.baidu.com/api?v=2.0&ak=e35biu9blkyBwcyfYBXaI5zfwyMRjLSU&callback=mapinit'//数据请求
+            'lodash': rootpath + '_assets/js/public/underscore/underscore.js'//基础库
+            , 'html5media': rootpath + '_assets/js/public/html5media/html5media-1.1.8.min.js'//多媒体
+            , 'moment': rootpath + '_assets/js/public/moment/moment-local-2.1.3.min.js'//日期
+            , 'validator': rootpath + '_assets/js/public/validator/validator-5.2.0.min.js'//验证
+            , 'common': rootpath + '_assets/js/module/common/common.js'//加载通用函数
+            , 'noty': rootpath + '_assets/js/module/notykit/notykit.js'//对话框
+            , 'dataload': rootpath + '_assets/js/module/dataload/dataload.js'//数据请求
+            , 'seajs': rootpath + '_assets/js/frame/seajs/sea.js'//
+            , 'seajs-css': rootpath + '_assets/js/frame/seajs/seajs-css.js'//
+            , 'seajs-preload': rootpath + '_assets/js/frame/seajs/seajs-preload.js'//数据请求
         },
         callback: function (url, result, key) {
             //alert(url);
@@ -58,7 +75,16 @@
         complete: function () {
             pageInit();
             sysSpinkit.remove();
-            sysSpinkit=null;
+            sysSpinkit = null;
+        }
+    },{
+        test: loadMapFlg,
+        yep: loadMapUrl,
+        callback: function (url, result, key) {
+            //alert(url);
+        },
+        complete: function () {
+            //alert('a');
         }
     }]);
 }());
